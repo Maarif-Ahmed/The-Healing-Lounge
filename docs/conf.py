545bda5477c9555b828d9c8fd52651c2cb47ddcd
Maarif-Ahmed <72371098+Maarif-Ahmed@users.noreply.github.com>
2024-03-22 13:21:29 +0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -37,8 +37,7 @@ master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+
 
 # -- Options for LaTeX output ------------------------------------------------
 
